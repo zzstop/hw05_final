dde61b201ef81b985e0bbf0fe417269e6902c44a
Ярослав <bankakonservov@mail.ru>
2021-01-19 18:14:45 +0500

    Correct media

--- yatube/urls.py
+++ yatube/urls.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.conf.urls import handler404, handler500
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('auth/', include('users.urls')),
@@ -23,3 +25,8 @@ if settings.DEBUG:
         settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     import debug_toolbar
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += [re_path(
+    r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
