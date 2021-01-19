c63e2300e08695a1d7945b573cfcdf7e5389a873
Ярослав <bankakonservov@mail.ru>
2021-01-19 14:53:25 +0500

    urls

--- yatube/urls.py
+++ yatube/urls.py
@@ -22,4 +22,4 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     import debug_toolbar
-    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
